1e68f5c1a9d7f767a39e23f8a9a6309c2749d67a
aaron levin <aaron.levin@soundcloud.com>
2016-01-10 15:45:31 +0100

    Experiment with Different Config Options

--- servant-server/src/Servant/Server/Internal/Config.hs
+++ servant-server/src/Servant/Server/Internal/Config.hs
@@ -1,13 +1,8 @@
 {-# LANGUAGE CPP                        #-}
 {-# LANGUAGE DataKinds                  #-}
-{-# LANGUAGE DeriveFoldable             #-}
-{-# LANGUAGE DeriveFunctor              #-}
-{-# LANGUAGE DeriveGeneric              #-}
-{-# LANGUAGE DeriveTraversable          #-}
 {-# LANGUAGE FlexibleInstances          #-}
 {-# LANGUAGE FunctionalDependencies     #-}
 {-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE KindSignatures             #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
 {-# LANGUAGE ScopedTypeVariables        #-}
@@ -19,16 +14,6 @@
 
 module Servant.Server.Internal.Config where
 
-import Control.DeepSeq (NFData(rnf))
-import GHC.Generics (Generic)
-import Data.Typeable (Typeable)
-
--- | A single entry in the configuration. The first parameter is phantom, and
--- is used to lookup a @ConfigEntry@ in a @Config@.
-newtype ConfigEntry tag a = ConfigEntry { unConfigEntry :: a }
-  deriving ( Eq, Show, Read, Enum, Integral, Fractional, Generic, Typeable
-           , Num, Ord, Real, Functor, Foldable, Traversable, NFData)
-
 -- | The entire configuration.
 data Config a where
     EmptyConfig :: Config '[]
@@ -36,8 +21,8 @@ data Config a where
 
 instance Show (Config '[]) where
   show EmptyConfig = "EmptyConfig"
-instance (Show a, Show (Config as)) => Show (Config (ConfigEntry tag a ': as)) where
-  showsPrec outerPrecedence (ConsConfig (ConfigEntry a) as) =
+instance (Show a, Show (Config as)) => Show (Config (a ': as)) where
+  showsPrec outerPrecedence (ConsConfig a as) =
     showParen (outerPrecedence > 5) $
       shows a . showString " .:. " . shows as
 
@@ -46,17 +31,17 @@ instance Eq (Config '[]) where
 instance (Eq a, Eq (Config as)) => Eq (Config (a ': as)) where
     ConsConfig x1 y1 == ConsConfig x2 y2 = x1 == x2 && y1 == y2
 
-(.:.) :: x -> Config xs -> Config (ConfigEntry tag x ': xs)
-e .:. cfg = ConsConfig (ConfigEntry e) cfg
+(.:.) :: x -> Config xs -> Config (x ': xs)
+e .:. cfg = ConsConfig e cfg
 infixr 5 .:.
 
-class HasConfigEntry (cfg :: [*]) (a :: k) (val :: *) | cfg a -> val where
-    getConfigEntry :: proxy a -> Config cfg -> val
+class HasConfigEntry (cfg :: [*]) (val :: *) | cfg -> val where
+    getConfigEntry :: Config cfg -> val
 
 instance OVERLAPPABLE_
-         HasConfigEntry xs tag val => HasConfigEntry (notIt ': xs) tag val where
-    getConfigEntry p (ConsConfig _ xs) = getConfigEntry p xs
+         HasConfigEntry xs val => HasConfigEntry (notIt ': xs) val where
+    getConfigEntry (ConsConfig _ xs) = getConfigEntry xs
 
 instance OVERLAPPABLE_
-         HasConfigEntry (ConfigEntry tag val ': xs) tag val where
-    getConfigEntry _ (ConsConfig x _) = unConfigEntry x
+         HasConfigEntry (val ': xs) val where
+    getConfigEntry (ConsConfig x _) = x
--- servant-server/test/Servant/Server/Internal/ConfigSpec.hs
+++ servant-server/test/Servant/Server/Internal/ConfigSpec.hs
@@ -1,54 +1,54 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# OPTIONS_GHC -fdefer-type-errors #-}
 module Servant.Server.Internal.ConfigSpec (spec) where
 
-import           Data.Proxy                     (Proxy (..))
-import           Test.Hspec                     (Spec, describe, it, shouldBe, pending, context)
+import           Test.Hspec                     (Spec, describe, it, shouldBe, context)
 import           Test.ShouldNotTypecheck        (shouldNotTypecheck)
+import           Control.DeepSeq                (NFData)
+
 
 import           Servant.API
 import           Servant.Server.Internal.Config
 
+newtype WrappedInt = WrappedInt Int deriving (Eq, Show, NFData)
+newtype WrappedChar = WrappedChar Char deriving (Eq, Show, NFData)
+
 spec :: Spec
-spec = do
-  getConfigEntrySpec
+spec = getConfigEntrySpec
 
 getConfigEntrySpec :: Spec
 getConfigEntrySpec = describe "getConfigEntry" $ do
 
-  let cfg1 = 0 .:. EmptyConfig :: Config '[ConfigEntry "a" Int]
-      cfg2 = 1 .:. cfg1 :: Config '[ConfigEntry "a" Int, ConfigEntry "a" Int]
+  let cfg1 = WrappedInt 0 .:. EmptyConfig :: Config '[WrappedInt]
+      cfg2 = 1 .:. cfg1 :: Config '[Int, WrappedInt]
 
-  it "gets the config if a matching one exists" $ do
+  it "gets the config if a matching one exists" $
+    getConfigEntry cfg1 `shouldBe` WrappedInt 0
 
-    getConfigEntry (Proxy :: Proxy "a") cfg1 `shouldBe` 0
-
-  it "gets the first matching config" $ do
-
-    getConfigEntry (Proxy :: Proxy "a") cfg2 `shouldBe` 1
+  it "gets the first matching config" $
+    getConfigEntry cfg2 `shouldBe` 1
 
   it "allows to distinguish between different config entries with the same type by tag" $ do
-    let cfg = 'a' .:. 'b' .:. EmptyConfig :: Config '[ConfigEntry 1 Char, ConfigEntry 2 Char]
-    getConfigEntry (Proxy :: Proxy 1) cfg `shouldBe` 'a'
+    let cfg = 'a' .:. WrappedChar 'b' .:. EmptyConfig :: Config '[Char, WrappedChar]
+    (getConfigEntry cfg :: Char) `shouldBe` 'a'
 
   context "Show instance" $ do
-    let cfg = 1 .:. 2 .:. EmptyConfig
-    it "has a Show instance" $ do
+    let cfg = (1 :: Integer) .:. (2 :: Integer) .:. EmptyConfig
+    it "has a Show instance" $
       show cfg `shouldBe` "1 .:. 2 .:. EmptyConfig"
 
-    it "bracketing works" $ do
+    it "bracketing works" $
       show (Just cfg) `shouldBe` "Just (1 .:. 2 .:. EmptyConfig)"
 
     it "bracketing works with operators" $ do
-      let cfg = (1 .:. 'a' .:. EmptyConfig) :<|> ('b' .:. True .:. EmptyConfig)
-      show cfg `shouldBe` "(1 .:. 'a' .:. EmptyConfig) :<|> ('b' .:. True .:. EmptyConfig)"
+      let cfg' = ((1 :: Integer) .:. 'a' .:. EmptyConfig) :<|> ('b' .:. True .:. EmptyConfig)
+      show cfg' `shouldBe` "(1 .:. 'a' .:. EmptyConfig) :<|> ('b' .:. True .:. EmptyConfig)"
 
-  it "does not typecheck if key does not exist" $ do
-
-    let x = getConfigEntry (Proxy :: Proxy "b") cfg1 :: Int
+  it "does not typecheck if type does not exist in config" $ do
+    let x = getConfigEntry cfg1 :: Char
     shouldNotTypecheck x
 
-  it "does not typecheck if key maps to a different type" $ do
-
-    let x = getConfigEntry (Proxy :: Proxy "a") cfg1 :: String
-    shouldNotTypecheck x
+  -- it "does not typecheck if key maps to a different type" $ do
+    -- let x = getConfigEntry  cfg1 :: String
+    -- shouldNotTypecheck x
