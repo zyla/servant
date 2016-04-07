125e089067f54e348379d11ede5189727093c780
Sönke Hahn <soenkehahn@gmail.com>
2016-04-07 21:13:43 +0800

    add stripIdentity

--- servant/src/Servant/Utils/Map.hs
+++ servant/src/Servant/Utils/Map.hs
@@ -3,18 +3,36 @@
 {-# LANGUAGE RankNTypes            #-}
 {-# LANGUAGE TypeFamilies          #-}
 {-# LANGUAGE TypeOperators         #-}
-module Servant.Utils.Map (mapLeaves) where
+module Servant.Utils.Map (mapLeaves, StripIdentity(..)) where
+
+import           Data.Functor.Identity
 
 import           Servant.API
 
 class Mappable s t a b where
   mapLeaves :: (forall x . a x -> b x) -> s -> t
 
+instance (Mappable left left' a b, Mappable right right' a b) => Mappable (left :<|> right) (left' :<|> right') a b where
+  mapLeaves f (left :<|> right) = mapLeaves f left :<|> mapLeaves f right
+
+instance Mappable s t a b => Mappable (arg -> s) (arg -> t) a b where
+  mapLeaves f s = mapLeaves f . s
+
 instance Mappable (a x) (b x) a b where
   mapLeaves f a = f a
 
-instance Mappable s t a b => Mappable (arg -> s) (arg -> t) a b where
-  mapLeaves f s = mapLeaves f $ s
+class StripIdentity a where
+  type Stripped a :: *
+  stripIdentity :: a -> Stripped a
 
-instance (Mappable left left' a b, Mappable right right' a b) => Mappable (left :<|> right) (left' :<|> right') a b where
-  mapLeaves f (left :<|> right) = mapLeaves f left :<|> mapLeaves f right
+instance (StripIdentity left, StripIdentity right) => StripIdentity (left :<|> right) where
+  type Stripped (left :<|> right) = Stripped left :<|> Stripped right
+  stripIdentity (left :<|> right) = stripIdentity left :<|> stripIdentity right
+
+instance StripIdentity b => StripIdentity (a -> b) where
+  type Stripped (a -> b) = a -> Stripped b
+  stripIdentity f = \ a -> stripIdentity (f a)
+
+instance StripIdentity (Identity a) where
+  type Stripped (Identity a) = a
+  stripIdentity (Identity a) = a
--- servant/test/Servant/Utils/MapSpec.hs
+++ servant/test/Servant/Utils/MapSpec.hs
@@ -1,3 +1,4 @@
+
 module Servant.Utils.MapSpec where
 
 import           Test.Hspec
@@ -5,23 +6,36 @@ import           Test.Hspec
 import           Data.Functor.Identity
 import           Servant.API
 import           Servant.Utils.Map
+
 spec :: Spec
-spec = describe "Map" $ do
-  it "maps " $ do
-    let foo :: Bool -> Int -> String
-        foo _ b = show b
+spec = do
+  describe "mapLeaves" $ do
+    it "maps leaves of fish structures" $ do
+      let foo :: Bool -> Int -> String
+          foo _ b = show b
 
-        bar :: () -> String -> Int -> Double
-        bar _ _ i = fromIntegral i
+          bar :: () -> String -> Int -> Double
+          bar _ _ i = fromIntegral i
 
-        foobar = foo :<|> bar
+          foobar = foo :<|> bar
 
-        convert :: (Int -> a) -> Identity a
-        convert f = Identity $ f 42
+          convert :: (Int -> a) -> Identity a
+          convert f = Identity $ f 42
 
-        foo' :: Bool -> Identity String
-        bar' :: () -> String -> Identity Double
-        foo' :<|> bar' = mapLeaves convert foobar
+          foo' :: Bool -> Identity String
+          bar' :: () -> String -> Identity Double
+          foo' :<|> bar' = mapLeaves convert foobar
 
-    foo' True `shouldBe` Identity "42"
-    bar' () "" `shouldBe` Identity (42 :: Double)
+      foo' True `shouldBe` Identity "42"
+      bar' () "" `shouldBe` Identity (42 :: Double)
+
+  describe "stripIdentity" $ do
+    it "works" $ do
+      let foo :: Identity String
+          foo = Identity "foo"
+          bar :: () -> Identity Bool
+          bar () = Identity True
+          foobar = foo :<|> bar
+          foo' :<|> bar' = stripIdentity foobar
+      foo' `shouldBe` ("foo" :: String)
+      bar' () `shouldBe` True
