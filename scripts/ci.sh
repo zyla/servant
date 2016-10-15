f5251b2e511d614fcfc3e27bf5822c96b9a5df19
Sönke Hahn <soenkehahn@gmail.com>
2016-10-15 18:06:11 -0400

    fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,5 +7,13 @@ set -o verbose
 stack test --ghc-options=-Werror --no-terminal --coverage
 
 # send test coverage statistics to coveralls
-STACK_YAML=stack.yaml stack install stack-hpc-coveralls-0.0.4.0 --no-terminal
+if ! which shc ; then
+  tmpDir=$(mktemp -d)
+  cd $tmpDir
+  wget https://github.com/rubik/stack-hpc-coveralls/releases/download/v0.0.4.0/shc-linux-x64-8.0.1.tar.bz2
+  tar -xf shc-linux-x64-8.0.1.tar.bz2
+  cp shc ~/.local/bin
+fi
+which shc
+
 shc combined all
