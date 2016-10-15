8dd5c2c128ff10e4accaa809d1885fe5dc8c8ed5
Sönke Hahn <soenkehahn@gmail.com>
2016-10-15 17:28:15 -0400

    fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -4,7 +4,7 @@ set -o nounset
 set -o errexit
 set -o verbose
 
-stack test --ghc-options=-Werror --no-terminal
+stack test --ghc-options=-Werror --no-terminal --coverage
 
 # send test coverage statistics to coveralls
 stack install stack-hpc-coveralls-0.0.4.0 --no-terminal
