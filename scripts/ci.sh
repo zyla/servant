560fddbbc66dda7740b4b462d149f1100ef999f1
Sönke Hahn <soenkehahn@gmail.com>
2016-10-15 17:16:19 -0400

    ci: send test coverage statistics to coveralls

---
+++ scripts/ci.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+set -o verbose
+
+stack test --ghc-options=-Werror --no-terminal
+
+# send test coverage statistics to coveralls
+stack install stack-hpc-coveralls-0.0.4.0 --no-terminal
+shc combined all
