9025929665d1c903948bc90e2da1a5c704105340
Sönke Hahn <soenkehahn@gmail.com>
2016-10-15 18:13:13 -0400

    fix

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,10 +9,10 @@ stack test --ghc-options=-Werror --no-terminal --coverage
 # send test coverage statistics to coveralls
 if ! which shc ; then
   tmpDir=$(mktemp -d)
-  cd $tmpDir
-  wget https://github.com/rubik/stack-hpc-coveralls/releases/download/v0.0.4.0/shc-linux-x64-8.0.1.tar.bz2
-  tar -xf shc-linux-x64-8.0.1.tar.bz2
-  cp shc ~/.local/bin
+  (cd $tmpDir &&
+   wget https://github.com/rubik/stack-hpc-coveralls/releases/download/v0.0.4.0/shc-linux-x64-8.0.1.tar.bz2 &&
+   tar -xf shc-linux-x64-8.0.1.tar.bz2 &&
+   cp shc ~/.local/bin)
 fi
 which shc
 
