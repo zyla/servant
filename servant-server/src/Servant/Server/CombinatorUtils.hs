7637446475b34eca17e5676edad066025ee4ce35
Sönke Hahn <soenkehahn@gmail.com>
2016-10-23 14:00:52 -0400

    add captureCombinator

--- servant-server/src/Servant/Server/CombinatorUtils.hs
+++ servant-server/src/Servant/Server/CombinatorUtils.hs
@@ -5,9 +5,11 @@
 module Servant.Server.CombinatorUtils (
   RouteResult(..),
   argumentCombinator,
+  captureCombinator,
 ) where
 
 import           Data.Proxy
+import           Data.Text
 import           Network.Wai
 
 import           Servant.API
@@ -26,3 +28,16 @@ argumentCombinator ::
 argumentCombinator getArg Proxy context delayed =
   route (Proxy :: Proxy api) context $ addBodyCheck delayed $
   DelayedIO $ \ request -> return $ getArg request
+
+captureCombinator ::
+  forall api combinator arg context env .
+  (HasServer api context) =>
+  (Text -> RouteResult arg)
+  -> Proxy (combinator :> api)
+  -> Context context
+  -> Delayed env (arg -> Server api)
+  -> Router' env RoutingApplication
+captureCombinator getArg Proxy context delayed =
+  CaptureRouter $
+  route (Proxy :: Proxy api) context $ addCapture delayed $ \ captured ->
+  DelayedIO $ \ request -> return $ getArg captured
